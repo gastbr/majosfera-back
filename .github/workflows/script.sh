fedaf19f9f109f6aaa923e284f92f62ec3e74a2e
AlumG <AlumG@majada.es>
2025-02-13 21:19:02 +0100

    FIX deploy, w/ Alberto

--- .github/workflows/script.sh
+++ .github/workflows/script.sh
@@ -10,8 +10,10 @@ echo 'npm install'; npm install
 
 echo 'npm run build'; npm run build
 
-echo 'artisan route:clear';php artisan route:clear
+echo 'chown'; chown -R www-data:www-data /var/www/html/majosfera-back
 
-echo 'php reload';sudo systemctl reload php8.2-fpm
+echo 'artisan route:clear'; php artisan route:clear
+
+echo 'php reload'; systemctl reload php8.2-fpm
 
 echo 'Deploy completed'
