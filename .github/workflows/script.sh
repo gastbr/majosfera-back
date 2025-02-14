cc7a5ee4c962be023a592f76e52bc0ae395dd3d3
AlumG <AlumG@majada.es>
2025-02-14 16:27:46 +0100

    FIX deploy, modified script, added chmod 755-775

--- .github/workflows/script.sh
+++ .github/workflows/script.sh
@@ -10,7 +10,11 @@ echo 'npm install'; npm install
 
 echo 'npm run build'; npm run build
 
-echo 'chown/chmod'; chown -R www-data:www-data /var/www/html/majosfera-back; chmod -R 777 /var/www/html/majosfera-back
+echo 'chown/chmod';
+chown -R www-data:www-data /var/www/html/majosfera-back;
+chmod -R 755 /var/www/html/majosfera-back;
+chmod -R 775 /var/www/html/majosfera-back/storage;
+chmod -R 775 /var/www/html/majosfera-back/bootstrap/cache;
 
 echo 'artisan route:clear'; php artisan route:clear
 
