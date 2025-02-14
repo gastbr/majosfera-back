4ce2712c6fad88b4e20deee434dd3bfe09b834d9
AlumG <AlumG@majada.es>
2025-02-14 17:05:58 +0100

    FIX deploy script format

--- .github/workflows/script.sh
+++ .github/workflows/script.sh
@@ -1,23 +1,23 @@
-echo 'Start deploy'
+echo '==> Start deploy'
 
-echo 'cd'; cd /var/www/html/majosfera-back
+echo '==> cd'; cd /var/www/html/majosfera-back
 
-echo 'git pull'; git restore .;git pull origin prod
+echo '==> git pull'; git restore .;git pull origin prod
 
-echo 'composer install'; sudo -u www-data composer install
+echo '==> composer install'; sudo -u www-data composer install
 
-echo 'npm install'; npm install
+echo '==> npm install'; npm install
 
-echo 'npm run build'; npm run build
+echo '==> npm run build'; npm run build
 
-echo 'chown/chmod';
+echo '==> chown / chmod';
 chown -R www-data:www-data /var/www/html/majosfera-back;
 chmod -R 755 /var/www/html/majosfera-back;
 chmod -R 775 /var/www/html/majosfera-back/storage;
 chmod -R 775 /var/www/html/majosfera-back/bootstrap/cache;
 
-echo 'artisan route:clear'; php artisan route:clear
+echo '==> artisan route:clear'; php artisan route:clear
 
-echo 'php reload'; systemctl reload php8.2-fpm
+echo '==> php reload'; systemctl reload php8.2-fpm
 
-echo 'Deploy completed'
+echo '==> Deploy completed'
