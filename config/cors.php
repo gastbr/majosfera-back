4ab1c296f29213f1e240256e1a5747afec306a84
Miriam <miriam@miriam.com>
2025-02-19 17:42:34 +0000

    FIX local front in cors

--- config/cors.php
+++ config/cors.php
@@ -34,7 +34,7 @@ return [
 
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:3000', 'https://majosfera-front.pages.dev', 'https://majosfera-front.vercel.app'],
+    'allowed_origins' => ['http://localhost:5173', 'https://majosfera-front.pages.dev', 'https://majosfera-front.vercel.app'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
