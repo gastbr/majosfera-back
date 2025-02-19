60231f5ad2651c58ee9784c6b17ab8ad803f46cf
Miriam <miriam@miriam.com>
2025-02-19 18:16:09 +0000

    ADD category controller

--- routes/api.php
+++ routes/api.php
@@ -4,9 +4,11 @@ use App\Http\Controllers\Api\ProductController;
 use Illuminate\Support\Facades\Route;
 use Orion\Facades\Orion;
 use App\Http\Controllers\Api\UserController;
+use App\Http\Controllers\Api\CategoryController;
 
 Route::group(['as' => 'api.'], function () {
     // Orion resource routes
     Orion::resource('users', UserController::class);
     Orion::resource('products', ProductController::class);
+    Orion::resource('categories', CategoryController::class);
 });
