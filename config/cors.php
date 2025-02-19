82f8c6f7b186a72af90dbfaffa497de20c85a0fd
AlumG <AlumG@majada.es>
2025-02-19 20:03:04 +0100

    OPS (auth:cors) added local url as origin

--- config/cors.php
+++ config/cors.php
@@ -32,16 +32,12 @@ return [
     'supports_credentials' => true,
 */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie', '*'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:3000', 'https://majosfera-front.pages.dev', 'https://majosfera-front.vercel.app'],
+    'allowed_origins' => ['http://localhost:3000', 'http://localhost:5174', 'https://majosfera-front.pages.dev', 'https://majosfera-front.vercel.app'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
     'max_age' => 0,
     'supports_credentials' => true,
-
-
-
-
 ];
