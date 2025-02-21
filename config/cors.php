fc1ad3aca457c89ae27f36584b27f6fa0530a486
gastbr <71907959+gastbr@users.noreply.github.com>
2025-02-21 06:27:45 +0000

    FIX cors modified

--- config/cors.php
+++ config/cors.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'paths' => ['*'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie', 'login', 'logout', 'register', 'email/verification-notification', 'forgot-password', 'reset-password', 'up', 'verify-email'],
 
     'allowed_methods' => ['*'],
 
