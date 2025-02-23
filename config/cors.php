0af66abdb03af179b851de9bc02100f0aed57abe
gastbr <71907959+gastbr@users.noreply.github.com>
2025-02-23 23:27:31 +0000

    FIX cors, exposed headers = set-cookie

--- config/cors.php
+++ config/cors.php
@@ -19,13 +19,13 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [env('FRONTEND_URL')],
+    'allowed_origins' => ['https://majosfera-front.pages.dev'],
 
     'allowed_origins_patterns' => [],
 
     'allowed_headers' => ['*'],
 
-    'exposed_headers' => [],
+    'exposed_headers' => ['Set-Cookie'],
 
     'max_age' => 0,
 
