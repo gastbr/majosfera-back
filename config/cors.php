f62c1cde41a43954f5790b02f7143b1a4480a279
gastbr <71907959+gastbr@users.noreply.github.com>
2025-02-23 23:14:10 +0000

    FIX cors allowed origins

--- config/cors.php
+++ config/cors.php
@@ -19,13 +19,13 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [env('FRONTEND_URL', 'https://localhost:5173')],
+    'allowed_origins' => [env('FRONTEND_URL')],
 
     'allowed_origins_patterns' => [],
 
     'allowed_headers' => ['*'],
 
-    'exposed_headers' => [true],
+    'exposed_headers' => [],
 
     'max_age' => 0,
 
