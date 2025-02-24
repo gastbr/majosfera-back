0e4828fca6a470e9b356a6588dc03be33c9c8cc6
AlumG <AlumG@majada.es>
2025-02-24 17:45:23 +0100

    FIX cors, changed allowed origins from env to several strings

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [env('SANCTUM_STATEFUL_DOMAINS')],
+    'allowed_origins' => ['miriam.informaticamajada.es', 'majosfera-front.pages.dev', 'majosfera-front.vercel.app', 'localhost', 'localhost:5173', '127.0.0.1', '127.0.0.1:5173'],
 
     'allowed_origins_patterns' => [],
 
