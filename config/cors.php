3c1ad8c5e4a4299aa586460aced0315a82346e87
Miriam <miriam@miriam.com>
2025-02-25 18:10:07 +0000

    Add link cors

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://majosfera-front.pages.dev', 'http://localhost:5173'],
+    'allowed_origins' => ['https://majosfera-front.pages.dev', 'http://localhost:5173', 'http://localhost:5174'],
 
     'allowed_origins_patterns' => [],
 
